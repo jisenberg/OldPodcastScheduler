c041114e2b027e29fad7e5b8724027c7b8822e3e
Joseph Isenberg <jisenberg@gmail.com>
2017-01-06 09:36:27 -0500

    Added code to pull rss from a url, parse and replace the items

---
+++ web.rb
@@ -0,0 +1,19 @@
+# myapp.rb
+require 'sinatra'
+require 'rss'
+require 'open-uri'
+require 'nokogiri'
+
+get '/' do
+  erb :index
+end
+
+post '/parse-rss' do
+  result = ""
+  items = Nokogiri::XML.fragment "<item>Item1</item><item>Item2</item>"
+  url = params[:url]
+  feed = Nokogiri::XML(open(url))
+  feed.xpath("//item").remove
+  feed.xpath("//channel")[0].add_child(items)
+  feed.to_xml
+end
